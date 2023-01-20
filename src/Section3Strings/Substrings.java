b391541af028857c5780d35e7279cb0bd6278e29
Adruti Onam <88138025+adrutionam@users.noreply.github.com>
2023-01-20 18:39:11 +0530

    create substrings.java
    creation

---
+++ src/Section3Strings/Substrings.java
@@ -0,0 +1,9 @@
+package Section3Strings;
+
+public class Substrings {
+    public static void main(String[] args) {
+        String myText = "apple";
+        String myNewText = myText.substring(0); // starts from the character we mentioned to
+        System.out.println(myNewText);
+    }
+}
