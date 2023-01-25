7115232fafc315cdb534283a5146ff366e15a83d
Adruti Onam <88138025+adrutionam@users.noreply.github.com>
2023-01-25 14:10:13 +0530

    new

---
+++ src/Section3Strings/Hello.java
@@ -0,0 +1,15 @@
+package Section3Strings;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        String name = "bijendra";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello " + name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+
+}
